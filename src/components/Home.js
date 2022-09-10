add399f366702bd3af8d95070ba9108a2c925226
ijeoma <ijeomaodinko@gmail.com>
2022-09-10 08:44:27 -0400

    Fix required changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { assetItem, assetFilter } from '../redux/Home';
 import Asset from './Asset';
 import './Home.css';
 
-const Home = () => {
+function Home() {
   const dispatch = useDispatch();
   const assetList = useSelector((state) => state.assetReducer);
+
+  const [searchCoin, setSearchCoin] = useState('');
   useEffect(() => {
     dispatch(assetItem());
-  }, []);
+  }, [searchCoin]);
+
+  const filterCoin = assetList.filter((coins) => (
+    coins.name.toLowerCase().includes(searchCoin.toLowerCase())));
 
   const changeHandler = (e) => {
+    const { value } = e.target;
+    setSearchCoin(value);
+  };
+
+  const taskHandler = (e) => {
     dispatch(assetFilter(e.target.value));
   };
 
@@ -19,15 +29,25 @@ const Home = () => {
     <section className="sectionDiv">
       <div className="optionDiv">
         <div className="option">
-          <select className="desc" onChange={changeHandler}>
+          <select className="desc" onChange={taskHandler}>
             <option value="price"> Rank </option>
             <option value="name"> Name </option>
             <option value="vol"> High Price </option>
           </select>
         </div>
       </div>
+      <div className="formDiv">
+        <input
+          type="text"
+          className="search"
+          name="coinSearch"
+          placeholder="Enter the coin..."
+          required
+          onChange={changeHandler}
+        />
+      </div>
       <ul className="assetList">
-        {assetList.map((coin) => (
+        {filterCoin.map((coin) => (
           <Asset
             key={coin.id}
             id={coin.id}
@@ -39,6 +59,6 @@ const Home = () => {
       </ul>
     </section>
   );
-};
+}
 
 export default Home;
