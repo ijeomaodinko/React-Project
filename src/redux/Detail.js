012d2d18224d730f8796deea2b15de3590bc56ed
Ijeoma Odinko <95123554+ijeomaodinko@users.noreply.github.com>
2022-09-10 09:21:10 -0400

    Update Detail.js

--- src/redux/Detail.js
+++ src/redux/Detail.js
@@ -1,7 +1,6 @@
 const apiUrl = 'https://api.coincap.io/v2/assets/';
 
 const FETCH_EACH = 'FETCH_EACH';
-// const initialState = [{ id: 'bitcoin', symbol: 'BTC' }];
 
 const fetchAssetById = async (id) => {
   const response = await fetch(`${apiUrl}${id}`, { method: 'GET' });
