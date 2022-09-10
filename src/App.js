c50f7434d6b3474370acaf06065a5454a11bea71
ijeoma <ijeomaodinko@gmail.com>
2022-09-10 10:09:01 -0400

    fix required changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Detail from './components/Detail';
